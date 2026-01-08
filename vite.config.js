927a1389a7d9e78a6b28607d8a4e1b863dfa1554
niyoxx <114039528+niyoxx@users.noreply.github.com>
2026-01-08 14:27:28 -0500

    Configured GH pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/fixnex-frontend/',
   server: {
     port: 4000,
   }
